09de00d02be87e6eecd48a76a5962a3c6d70cffb
AnshulMenaria <anshulmenaria@gmail.com>
2024-09-28 18:23:36 +0530

    as

--- Admin-Panel/src/App.js
+++ Admin-Panel/src/App.js
@@ -1,6 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import AdminPanel from './Components/Admin-Panel';
-import NavigationBar from './Components/Navbar';
 import Login from './Components/Admin-Login';
 import AllContacts from './Components/Contact';
 import Reviews from './Components/Review';
