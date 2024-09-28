f69bba5e4657b66916c29059afdf8538eb567569
AnshulMenaria <anshulmenaria@gmail.com>
2024-09-28 16:50:27 +0530

    vroken

--- Admin-Panel/src/App.js
+++ Admin-Panel/src/App.js
@@ -1,23 +1,25 @@
-import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import AdminPanel from './Components/Admin-Panel';
-import NavigationBar from './Components/Navbar';
 import Login from './Components/Admin-Login';
 import AllContacts from './Components/Contact';
 import Reviews from './Components/Review';
 import UpdateAdmin from './Components/Setting';
+import Layout from './Components/Layout';
 
 function App() {
-  const location = useLocation(); // Get current location
 
   return (
     <>
-      {location.pathname !== '/' && <NavigationBar />} {/* Render NavigationBar only if not on the login page */}
+
       <Routes>
+        <Route path="/" element={<Layout/>}>
+
         <Route index element={<Login />} />
         <Route path='/adminpanel' element={<AdminPanel />} />
         <Route path='/contact' element={<AllContacts />} />
         <Route path='/reviews' element={<Reviews />} />
         <Route path='/setting' element={<UpdateAdmin />} />
+        </Route>
       </Routes>
     </>
   );
--- Admin-Panel/src/Components/Layout.jsx
+++ Admin-Panel/src/Components/Layout.jsx
@@ -1,20 +1,23 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom';
+import React from 'react';
+import { Outlet, useLocation } from 'react-router-dom';
 import NavigationBar from './Navbar.jsx';
 
 const Layout = () => {
-  return (
-    <>
-        <div>
-          <div className="app-container">
-            <NavigationBar />
-              <div className="main-content">
-                <Outlet/>
-              </div>
-          </div>
-        </div>
-    </> 
-  )
-}
+  const location = useLocation(); // Get the current location (route)
 
-export default Layout
+  return (
+    <div>
+      <div className="app-container">
+        {/* Conditionally render the Navbar */}
+        {location.pathname !== '/' && <NavigationBar />} {/* Hide Navbar on login page */}
+
+        {/* Main content */}
+        <div className="main-content">
+          <Outlet />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Layout;
